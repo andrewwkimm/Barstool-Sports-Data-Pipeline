6ae8a2a9ea20828b350ac00b2d2b0b42aef6a315
andrewwkimm <andrewkimka@gmail.com>
2025-05-26 08:14:14 +0000

    Add utility module to read blobs from GCS

---
+++ src/utils/gcs_file_reader.py
@@ -0,0 +1,21 @@
+"""The Google Cloud Storage File Reader."""
+
+import io
+import os
+from typing import BinaryIO
+
+from dotenv import load_dotenv
+from google.cloud import storage
+
+
+def read_gcs_file(bucket: str, blob_name: str) -> BinaryIO:
+    """Reads a file from Google Cloud Storage and returns it as a binary stream."""
+    load_dotenv()
+    SERVICE_ACCOUNT_KEY = os.getenv("SERVICE_ACCOUNT_KEY")
+
+    client = storage.Client.from_service_account_json(SERVICE_ACCOUNT_KEY)
+    bucket_obj = client.bucket(bucket)
+    blob = bucket_obj.blob(blob_name)
+    content = blob.download_as_bytes()
+    data = io.BytesIO(content)
+    return data
