d78afa45e0c1692de45a973d65d11b9d7ea944a6
andrewwkimm <andrewkimka@gmail.com>
2025-05-26 08:18:09 +0000

    Expose GCS and JSOL file reader functions

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,9 +1,13 @@
 """Utilites for the Barstool Sports Data Pipeline."""
 
-from .html_reader import read_html
+from .gcs_file_reader import read_gcs_file
+from .html_reader import read_html_file
+from .jsonl_reader import read_jsonl_file
 from .s3_file_reader import read_s3_file
 
 all = [
-    read_html,
+    read_gcs_file,
+    read_html_file,
+    read_jsonl_file,
     read_s3_file,
 ]
