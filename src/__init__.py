52b2311f7815894932653a4a36993297e6a1c6ef
andrewwkimm <andrewkimka@gmail.com>
2025-05-26 08:29:02 +0000

    Move module location

--- src/__init__.py
+++ src/__init__.py
@@ -1,3 +1,15 @@
 """The Barstool Sports Data Pipeline."""
 
 __version__ = "0.1.0"
+
+from .gcs_file_reader import read_gcs_file
+from .html_reader import read_html_file
+from .jsonl_reader import read_jsonl_file
+from .s3_file_reader import read_s3_file
+
+all = [
+    read_gcs_file,
+    read_html_file,
+    read_jsonl_file,
+    read_s3_file,
+]
--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,13 +1,1 @@
 """Utilites for the Barstool Sports Data Pipeline."""
-
-from .gcs_file_reader import read_gcs_file
-from .html_reader import read_html_file
-from .jsonl_reader import read_jsonl_file
-from .s3_file_reader import read_s3_file
-
-all = [
-    read_gcs_file,
-    read_html_file,
-    read_jsonl_file,
-    read_s3_file,
-]
